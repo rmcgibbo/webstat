64e933756bdea1deca7b34faec9c2752336d68ed
Robert McGibbon <rmcgibbo@gmail.com>
2012-12-07 01:39:55 -0800

    comment

--- fabfile.py
+++ fabfile.py
@@ -2,9 +2,12 @@ from fabric.api import *
 env.hosts = ['vsp-compute']
 
 def deploy():
+    "Start the daemon on the remote pbs servers"
+
     with cd('webstat'):
         run('git pull')
         run('python scripts/cluster_daemon.py')
 
 def coffee():
-    local('coffee -c webstat/assets/main.coffee')
+    "Compile the coffeescript to static javascript"
+    local('coffee -c -o webstat/static/ webstat/assets/main.coffee')
